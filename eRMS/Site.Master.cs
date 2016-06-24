83d8c9de33a22989d350db4e9f53eed42b7c64b2
x0223402 - new laptop <ismaeljaycarpio1992@gmail.com>
2016-06-24 15:33:36 +0800

    updated master page

--- eRMS/Site.Master.cs
+++ eRMS/Site.Master.cs
@@ -73,7 +73,7 @@ namespace eRMS
 
         protected void Unnamed_LoggingOut(object sender, LoginCancelEventArgs e)
         {
-            Context.GetOwinContext().Authentication.SignOut();
+            FormsAuthentication.SignOut();
         }
     }
 
