61e1ef41f5317ac5cc406db2fb151fb74877a3c5
x0223402 - new laptop <ismaeljaycarpio1992@gmail.com>
2016-06-24 11:46:55 +0800

    add required css and js files, added class lib

---
+++ eRMS/Account/Login.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="Login" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="eRMS.Account.Login" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+</asp:Content>
--- eRMS/Site.Master.designer.cs
+++ eRMS/Site.Master.designer.cs
@@ -13,6 +13,15 @@ namespace eRMS {
     public partial class SiteMaster {
         
         /// <summary>
+        /// SiteMapPath1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SiteMapPath SiteMapPath1;
+        
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
